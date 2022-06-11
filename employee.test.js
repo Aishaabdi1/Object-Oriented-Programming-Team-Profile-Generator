979b9e6239c76e9d8b346a1e1c0975885f6a17c5
Aisha Abdi <aishahajisirad@gmail.com>
2022-06-11 17:47:14 +0100

    test.js

---
+++ employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee ('Aisha', 0001, 'aishahajisirad@gmail.com');
+
+test('test if we can get the constructor values for the employee object', () => {
+expect(employee.name).toBe('Aisha');
+expect(employee.id).toBe(0001);
+expect(employee.email).toBe('aishahajisirad@gmail.com');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(employee.getName()).toBe('Aisha');
+});
+
+test('test if we can get the id from the getId() method', ()=> {
+    expect(employee.getId()).toBe('0001');
+});
+
+test('test if we can get the email from the getEmail() method', ()=> {
+    expect(employee.getEmail()).toBe('aishahajisirad@gmail.com');
+});
+
+
