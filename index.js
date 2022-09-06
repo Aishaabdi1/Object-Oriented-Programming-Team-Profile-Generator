ae52c10ef0521aee29545eb2cb51499fa45905ff
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-06 23:48:59 +0100

    creating function startApp

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require("path");
 const fs = require("fs");
 
 
+// Creating objects with different employee types
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const generateTeam = require("./src/template.js")
+
+teamArray = [];
+
+function startApp () {
+    
+}
